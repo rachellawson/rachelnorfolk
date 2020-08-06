4a19bf75dfe8fcbdcb814f204635d97fde0b4c32
Rachel Lawson <rachellawson@me.com>
2020-08-06 10:33:35 +0100

    In case we use ddev locally, check for ddev settings

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -43,3 +43,8 @@ if (file_exists($app_root . '/' . $site_path . '/settings.platformsh.php')) {
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
+
+// Automatically generated include for settings managed by ddev.
+if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
+  include $app_root . '/' . $site_path . '/settings.ddev.php';
+}
