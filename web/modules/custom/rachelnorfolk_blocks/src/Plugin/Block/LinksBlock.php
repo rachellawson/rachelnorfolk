4a31a629a24257497a4546e22dec5e3c004f7466
Rachel Lawson <rachellawson@me.com>
2021-10-30 14:18:02 +0100

    Add rachelnorfolk_blocks custom module to display some links in social area

---
+++ web/modules/custom/rachelnorfolk_blocks/src/Plugin/Block/LinksBlock.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\rachelnorfolk_blocks\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides an example block.
+ *
+ * @Block(
+ *   id = "rachelnorfolk_blocks_links",
+ *   admin_label = @Translation("Links Block"),
+ *   category = @Translation("rachelnorfolk blocks")
+ * )
+ */
+class LinksBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return ['label_display' => FALSE];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return [
+      '#theme' => 'links_block',
+      '#data' => [
+        'LinkedIn' => 'https://www.linkedin.com/in/rachel-norfolk',
+        'Twitter' => 'https://twitter.com/rachel_norfolk',
+        ],
+    ];
+  }
+
+}
