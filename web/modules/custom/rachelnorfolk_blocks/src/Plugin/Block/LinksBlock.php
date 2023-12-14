5922cca2be21981ad9a1f8ab718a0db24cb812c6
Rachel Lawson <rachellawson@me.com>
2023-12-14 18:40:10 +0000

    Update LinksBlock.php
    Adding in new Mastodon home

--- web/modules/custom/rachelnorfolk_blocks/src/Plugin/Block/LinksBlock.php
+++ web/modules/custom/rachelnorfolk_blocks/src/Plugin/Block/LinksBlock.php
@@ -30,7 +30,7 @@ class LinksBlock extends BlockBase {
       '#theme' => 'links_block',
       '#data' => [
         'LinkedIn' => 'https://www.linkedin.com/in/rachel-norfolk',
-        'Mastodon' => 'https://strangeobject.space/@rachel_norfolk'
+        'Mastodon' => 'https://norfolk.social/@rachel'
         ],
     ];
   }
