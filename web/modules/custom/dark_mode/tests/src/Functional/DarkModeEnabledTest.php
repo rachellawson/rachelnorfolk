627583320b5d1f4b018fae13294c9463196015b7
Rachel Lawson <rachellawson@me.com>
2024-01-30 18:57:45 +0000

    Staging some CI tests as example (#108)
    * Staging some CI tests as example

---
+++ web/modules/custom/dark_mode/tests/src/Functional/DarkModeEnabledTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\Tests\dark_mode\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Check that we are actually putting the css on the page.
+ *
+ * @group dark_mode
+ */
+final class DarkModeEnabledTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'olivero';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = ['dark_mode'];
+
+  /**
+   * Test callback.
+   */
+  public function testLibraryLoaded(): void {
+    $this->drupalGet('/');
+    $this->assertSession()->statusCodeEquals(200);
+
+    // Should contain the dark mode css but not that for grey pride.
+    $this->assertSession()->responseContains('dark_mode/css/dark-mode.css');
+    $this->assertSession()->responseNotContains('dark_mode/css/grey-pride.css');
+  }
+
+}
